194e2a4b5cc000ee26147c96185b2d837ed7dcc6
Errol Lagera <lagera.errol@gmail.com>
2023-05-16 14:02:22 +0800

    Test CSV database

--- website/models.py
+++ website/models.py
@@ -1,11 +1,14 @@
 from django.db import models
+from django.utils.translation import gettext as _
+from django.conf import settings
 
 class Record(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    phone_num = models.CharField(max_length=50)
-    customer_num = models.CharField(max_length=50)
-    name = models.CharField(max_length=50)
-    address = models.CharField(max_length=50)
+    created_at = models.DateTimeField(_("publication date"))
+    phone_num = models.CharField(max_length=100)
+    customer_num = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    plan = models.CharField(max_length=100, null=True)
 
     def __str__(self):
         return(f"{self.phone_num}")
